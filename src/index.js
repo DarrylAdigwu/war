e0d270e4f538ac0005ddc00ebac89121e535d5d7
DarrylAdigwu <115912161+DarrylAdigwu@users.noreply.github.com>
2026-01-03 19:41:26 -0800

    use deckofcards api to add a deck, draw cards, and handle game result logic

--- src/index.js
+++ src/index.js
@@ -1,68 +1,100 @@
-let deckId
-let computerScore = 0
-let myScore = 0
-const cardsContainer = document.getElementById("cards")
-const newDeckBtn = document.getElementById("new-deck")
-const drawCardBtn = document.getElementById("draw-cards")
-const header = document.getElementById("header")
-const remainingText = document.getElementById("remaining")
-const computerScoreEl = document.getElementById("computer-score")
-const myScoreEl = document.getElementById("my-score")
+let deckId = localStorage.getItem("deckId")
+const newDeck = document.getElementById("get-new-deck");
+const drawCardsBtn = document.getElementById("draw-cards");
+const cardContainer = document.getElementById("card-container");
+const remaining = document.getElementById("remaining");
+const computerScore = document.getElementById("comp-score");
+const userScore = document.getElementById("user-score");
+const statusMessage = document.getElementById("status-message")
+let compCount = 0;
+let userCount = 0;
 
-function handleClick() {
-    fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
-        .then(res => res.json())
-        .then(data => {
-            remainingText.textContent = `Remaining cards: ${data.remaining}`
-            deckId = data.deck_id
-            console.log(deckId)
-        })
+const getNewDeck = async function (){
+  const response = await fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
+  const data = await response.json();
+
+  remaining.textContent = `Remaining cards: ${data.remaining}`;
+  localStorage.setItem("deckId", data.deck_id)
+  deckId = data.deck_id
+  
+  compCount = 0;
+  userCount = 0
+  computerScore.textContent = `Computer: 0`;
+  userScore.textContent = `Me: 0`;
 }
 
-newDeckBtn.addEventListener("click", handleClick)
+newDeck.addEventListener("click", getNewDeck);
 
-drawCardBtn.addEventListener("click", () => {
-    fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
-        .then(res => res.json())
-        .then(data => {
-            remainingText.textContent = `Remaining cards: ${data.remaining}`
-            cardsContainer.children[0].innerHTML = `
-                <img src=${data.cards[0].image} class="card" />
-            `
-            cardsContainer.children[1].innerHTML = `
-                <img src=${data.cards[1].image} class="card" />
-            `
-            const winnerText = determineCardWinner(data.cards[0], data.cards[1])
-            header.textContent = winnerText
-            
-            if (data.remaining === 0) {
-                drawCardBtn.disabled = true
-                if (computerScore > myScore) {
-                    header.textContent = "The computer won the game!"
-                } else if (myScore > computerScore) {
-                    header.textContent = "You won the game!"
-                } else {
-                    header.textContent = "It's a tie game!"
-                }
-            }
-        })
-})
+const drawCards = async function(){
+  const response = await fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
+  const data = await response.json()
 
-function determineCardWinner(card1, card2) {
-    const valueOptions = ["2", "3", "4", "5", "6", "7", "8", "9", 
-    "10", "JACK", "QUEEN", "KING", "ACE"]
-    const card1ValueIndex = valueOptions.indexOf(card1.value)
-    const card2ValueIndex = valueOptions.indexOf(card2.value)
-    
-    if (card1ValueIndex > card2ValueIndex) {
-        computerScore++
-        computerScoreEl.textContent = `Computer score: ${computerScore}`
-        return "Computer wins!"
-    } else if (card1ValueIndex < card2ValueIndex) {
-        myScore++
-        myScoreEl.textContent = `My score: ${myScore}`
-        return "You win!"
+  const computerCard = data.cards[0];
+  const playerCard = data.cards[1];
+
+  if(data.remaining === 0) {
+    if(compCount > userCount) {
+      statusMessage.textContent = "Computer Won the Game!"
+    } else if (userCount > compCount) {
+      statusMessage.textContent = "You won the Game!"
     } else {
-        return "War!"
+      statusMessage.textContent = "It's a tie..."
     }
+
+    statusMessage.textContent = result;
+    drawCardsBtn.setAttribute("disabled", true)
+  }
+
+  remaining.textContent = `Remaining cards: ${data.remaining}`;
+
+  cardContainer.children[0].innerHTML = `
+    <img src="${computerCard.image}" />
+  `
+  cardContainer.children[1].innerHTML = `
+    <img src=${playerCard.image} />
+  `
+
+  const winner = getWinningCard(computerCard, playerCard);
+  statusMessage.innerHTML = `${winner}`
+}
+
+drawCardsBtn.addEventListener("click", drawCards)
+
+function getWinningCard(compCard, userCard) {
+  const currentCards = [compCard.value, userCard.value];
+  
+  currentCards.map((card, index) => {
+    switch(card) {
+      case "JACK":
+        currentCards[index] = 11;
+        break;
+      case "QUEEN":
+        currentCards[index] = 12;
+        break;
+      case "KING":
+        currentCards[index] = 13;
+        break;
+      case "ACE":
+        currentCards[index] = 14;
+        break;
+      default:
+        currentCards[index] = Number(card);
+    }
+  })
+  
+  const highCard = currentCards.reduce((initial, currentCard) => {
+     if(initial > currentCard) {
+      compCount++
+      computerScore.textContent = `Computer: ${compCount}`
+      return `Computer wins!`
+     } else if (currentCard > initial) {
+      userCount++
+      userScore.textContent = `Me: ${userCount}`
+      return `You win!`
+     } else {
+      return "War!"
+     }
+  })
+
+  return highCard;
 }
